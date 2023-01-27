b28f11ed5012b7364920150dd2656fbaf69ab53e
Ross Jackson <ross.santos@gmail.com>
2023-01-27 17:51:07 -0500

    Add a basic layout and trigger for Observable

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,32 @@
-import React from 'react'
+import React, { useEffect, useRef, useState } from 'react'
+import { getWeather, WeatherResponseProps } from 'weather'
 import './App.scss'
 
 function App() {
+   const [weatherData, setWeatherData] = useState<WeatherResponseProps | null>(null)
+   const renderedRef = useRef<boolean>(false)
+
+   useEffect(() => {
+      if (renderedRef.current) return
+      console.log('rendered')
+
+      const subscription = getWeather({
+         latitude: 38.8937335,
+         longitude: -77.0847874,
+         temperatureUnit: 'fahrenheit',
+      }).subscribe((data) => {
+         setWeatherData(data)
+
+         renderedRef.current = true
+      })
+
+      return () => {
+         subscription.unsubscribe()
+      }
+   }, [])
+
+   console.log(weatherData)
+
    return (
       <div className="App">
          <header className="App-header">
