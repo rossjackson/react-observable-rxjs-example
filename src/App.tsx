6f76b4ea69bf4d3c420231f8bf58d47af951fd75
Ross Jackson <ross.santos@gmail.com>
2023-01-30 22:43:32 -0500

    final observable

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,15 @@
 import React, { ChangeEvent, useEffect, useState } from 'react'
+import { catchError, concatMap, map, of, switchMap, tap } from 'rxjs'
+import { fromFetch } from 'rxjs/fetch'
 import {
    defaultLocationKey,
    defaultTemperatureUnit,
-   LatLongProps,
    locations,
    TemperatureUnitProps,
+   Weather,
    WeatherResponseProps,
 } from 'weatherHelper'
+import { weatherRequest$ } from 'weatherObservable'
 import WeatherResultComponent from 'WeatherResultComponent'
 import './App.scss'
 
@@ -16,9 +19,9 @@ const App = () => {
       success: false,
    })
    const [loading, setLoading] = useState<boolean>(false)
-   const [location, setLocation] = useState<LatLongProps>(locations[defaultLocationKey])
-   const [temperatureUnit, setTemperatureUnit] =
-      useState<TemperatureUnitProps>(defaultTemperatureUnit)
+   // const [location, setLocation] = useState<LatLongProps>(locations[defaultLocationKey])
+   // const [temperatureUnit, setTemperatureUnit] =
+   //    useState<TemperatureUnitProps>(defaultTemperatureUnit)
 
    // const { fetching, error, weatherSubject } = useWeather({
    //    defaultLocationKey,
@@ -26,101 +29,101 @@ const App = () => {
    //    setResult,
    // })
 
-   useEffect(() => {
-      const abortController = new AbortController()
-      const { latitude, longitude } = location
-      setLoading(true)
-      fetch(
-         `https://api.open-meteo.com/v1/forecast?latitude=${latitude}&longitude=${longitude}&current_weather=true&daily=temperature_2m_min,temperature_2m_max&temperature_unit=${temperatureUnit}&timezone=${
-            new Intl.DateTimeFormat().resolvedOptions().timeZone
-         }`,
-         { signal: abortController.signal }
-      )
-         .then((response) => {
-            if (!response.ok) {
-               throw new Error('API error')
-            }
-            return response.json()
-         })
-         .then((response) => {
-            setResult({
-               success: true,
-               weather: response,
-            })
-         })
-         .catch(() => {
-            setResult({
-               success: false,
-            })
-         })
-         .finally(() => {
-            setLoading(false)
-         })
-
-      return () => abortController.abort()
-   }, [temperatureUnit, location])
-
    // useEffect(() => {
-   //    const sub = weatherRequest$
-   //       .pipe(
-   //          tap(() => setLoading(true)),
-   //          switchMap(({ latitude, longitude, temperatureUnit }) =>
-   //             fromFetch(
-   //                `https://api.open-meteo.com/v1/forecast?latitude=${latitude}&longitude=${longitude}&current_weather=true&daily=temperature_2m_min,temperature_2m_max&temperature_unit=${temperatureUnit}&timezone=${
-   //                   new Intl.DateTimeFormat().resolvedOptions().timeZone
-   //                }`
-   //             ).pipe(
-   //                tap((response) => {
-   //                   if (!response.ok) {
-   //                      throw new Error('API error')
-   //                   }
-   //                }),
-   //                concatMap<Response, Promise<Weather>>((response) => response.json()),
-   //                map(
-   //                   (response) =>
-   //                      ({
-   //                         success: true,
-   //                         weather: response,
-   //                      } as WeatherResponseProps)
-   //                ),
-   //                catchError(() => {
-   //                   return of<WeatherResponseProps>({
-   //                      success: false,
-   //                   })
-   //                }),
-   //                tap(() => setLoading(false))
-   //             )
-   //          )
-   //       )
-   //       .subscribe((res) => setResult(res))
-   //    return () => sub.unsubscribe()
-   // }, [])
+   //    const abortController = new AbortController()
+   //    const { latitude, longitude } = location
+   //    setLoading(true)
+   //    fetch(
+   //       `https://api.open-meteo.com/v1/forecast?latitude=${latitude}&longitude=${longitude}&current_weather=true&daily=temperature_2m_min,temperature_2m_max&temperature_unit=${temperatureUnit}&timezone=${
+   //          new Intl.DateTimeFormat().resolvedOptions().timeZone
+   //       }`,
+   //       { signal: abortController.signal }
+   //    )
+   //       .then((response) => {
+   //          if (!response.ok) {
+   //             throw new Error('API error')
+   //          }
+   //          return response.json()
+   //       })
+   //       .then((response) => {
+   //          setResult({
+   //             success: true,
+   //             weather: response,
+   //          })
+   //       })
+   //       .catch(() => {
+   //          setResult({
+   //             success: false,
+   //          })
+   //       })
+   //       .finally(() => {
+   //          setLoading(false)
+   //       })
 
-   // const handleSelectionChange = (e: ChangeEvent<HTMLSelectElement>) => {
-   //    if (!weatherRequest$) return
-   //    const { latitude, longitude } = locations[e.currentTarget.value]
-   //    weatherRequest$.next({
-   //       ...weatherRequest$.value,
-   //       latitude,
-   //       longitude,
-   //    })
-   // }
+   //    return () => abortController.abort()
+   // }, [temperatureUnit, location])
+
+   useEffect(() => {
+      const sub = weatherRequest$
+         .pipe(
+            tap(() => setLoading(true)),
+            switchMap(({ latitude, longitude, temperatureUnit }) =>
+               fromFetch(
+                  `https://api.open-meteo.com/v1/forecast?latitude=${latitude}&longitude=${longitude}&current_weather=true&daily=temperature_2m_min,temperature_2m_max&temperature_unit=${temperatureUnit}&timezone=${
+                     new Intl.DateTimeFormat().resolvedOptions().timeZone
+                  }`
+               ).pipe(
+                  tap((response) => {
+                     if (!response.ok) {
+                        throw new Error('API error')
+                     }
+                  }),
+                  concatMap<Response, Promise<Weather>>((response) => response.json()),
+                  map(
+                     (response) =>
+                        ({
+                           success: true,
+                           weather: response,
+                        } as WeatherResponseProps)
+                  ),
+                  catchError(() => {
+                     return of<WeatherResponseProps>({
+                        success: false,
+                     })
+                  }),
+                  tap(() => setLoading(false))
+               )
+            )
+         )
+         .subscribe((res) => setResult(res))
+      return () => sub.unsubscribe()
+   }, [])
 
    const handleSelectionChange = (e: ChangeEvent<HTMLSelectElement>) => {
-      setLocation(locations[e.currentTarget.value])
+      if (!weatherRequest$) return
+      const { latitude, longitude } = locations[e.currentTarget.value]
+      weatherRequest$.next({
+         ...weatherRequest$.value,
+         latitude,
+         longitude,
+      })
    }
-
-   // const handleTemperatureChange = (e: ChangeEvent<HTMLSelectElement>) => {
-   //    if (!weatherRequest$) return
-   //    weatherRequest$.next({
-   //       ...weatherRequest$.value,
-   //       temperatureUnit: e.currentTarget.value as TemperatureUnitProps,
-   //    })
-   // }
 
    const handleTemperatureChange = (e: ChangeEvent<HTMLSelectElement>) => {
-      setTemperatureUnit(e.currentTarget.value as TemperatureUnitProps)
+      if (!weatherRequest$) return
+      weatherRequest$.next({
+         ...weatherRequest$.value,
+         temperatureUnit: e.currentTarget.value as TemperatureUnitProps,
+      })
    }
+
+   // const handleSelectionChange = (e: ChangeEvent<HTMLSelectElement>) => {
+   //    setLocation(locations[e.currentTarget.value])
+   // }
+
+   // const handleTemperatureChange = (e: ChangeEvent<HTMLSelectElement>) => {
+   //    setTemperatureUnit(e.currentTarget.value as TemperatureUnitProps)
+   // }
 
    console.log('render', loading)
 
